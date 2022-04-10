c6d81b43a911e4bd2ea278d2ec7dd3d7bf82a19a
GLuna1982 <gluna0705@gmail.com>
2022-04-10 16:53:40 -0400

    updated README file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,17 +1,13 @@
-var 
-// Sends form information to Google Sheet
 var form = document.getElementById('reservation');
-form.addEventListener("submit",function(event){
+
+form.addEventListener("submit", function(event){
     event.preventDefault();
-    fetch(form.action,{
+    fetch(form.action, {
         method: "POST",
-        body: new FormData(document.getElementById('reservation'))
-    }).then(()=>{
-        document.getElementById('message').innerHTML="Thank you, your reservation is set";
+        body: new FormData(document.getElementById('reservation')),
+    }).then(
+        response => response.json()
+    ).then((html)=>{
+        document.getElementById('message').innerHTML = "Your reservation has been made.";
     })
-});
-
-function createEvent () {
-    const calendar = '853474722699-6gmq2orobce9vte5bv48qsmuvgutob77.apps.googleusercontent.com';
-
-}
+})
